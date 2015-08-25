31ff0f539f12a6e8df8847134144576a611c53f0
delphino <delphino@31b658cd-b084-5547-90eb-9404aba538b9>
2015-08-25 15:20:39 +0000

    ImageButton revised

--- org.cryptocator/src/org/cryptocator/ImageLabelButton.java
+++ org.cryptocator/src/org/cryptocator/ImageLabelButton.java
@@ -34,128 +34,230 @@
 package org.cryptocator;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.ImageButton;
 
+/**
+ * The ImageLabelButton class is an ImageButton that has an additional label
+ * below the image icon. To use the ImageButton setTextAndImageResource() must
+ * be called programmatically.
+ * 
+ * @author Christian Motika
+ * @since 1.2
+ * @date 08/23/2015
+ * 
+ */
 public class ImageLabelButton extends ImageButton {
 
+	/** The label text. */
 	String text = "";
-	int textColor = Color.WHITE;
+
+	/** The default label text color. */
+	final int DEFAULTTEXTCOLOR = Color.WHITE;
+
+	/** The label text size. */
 	int textSize = 22;
+
+	/** The height of the text label. */
 	int height = 0;
 
+	/** The width of the text label. */
+	int width = 0;
+
+	/** The paint object for painting the additional label text. */
+	Paint paint = null;
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Instantiates a new image label button.
+	 * 
+	 * @param context
+	 *            the context
+	 */
 	public ImageLabelButton(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
 	}
 
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Instantiates a new image label button.
+	 * 
+	 * @param context
+	 *            the context
+	 * @param attrs
+	 *            the attrs
+	 */
 	public ImageLabelButton(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
 	}
 
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Instantiates a new image label button.
+	 * 
+	 * @param context
+	 *            the context
+	 * @param attrs
+	 *            the attrs
+	 * @param defStyle
+	 *            the def style
+	 */
 	public ImageLabelButton(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
 	}
 
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Sets the text and image resource for the ImageLabelButton with a default
+	 * font size of 22.
+	 * 
+	 * @param text
+	 *            the text
+	 * @param resId
+	 *            the res id
+	 */
 	public void setTextAndImageResource(String text, int resId) {
-		setTextAndImageResource( text, 22,  resId);
+		recalculateTextSize(text, textSize);
+		setTextAndImageResource(text, 22, resId);
 	}
 
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Sets the text and image resource for the ImageLabelButton.
+	 * 
+	 * @param text
+	 *            the text
+	 * @param textSize
+	 *            the text size
+	 * @param resId
+	 *            the res id
+	 */
 	public void setTextAndImageResource(String text, int textSize, int resId) {
 		this.text = text;
-		setImageResourceInternal(resId); 
+		this.textSize = textSize;
+		recalculateTextSize(text, textSize);
+		setImageResourceInternal(resId);
 	}
 
-	public void setTextSize(int size) {
-		this.textSize = size;
-	}
+	// ------------------------------------------------------------------------
 
-	public void setTextColor(int color) {
-		this.textColor = color;
-	}
-
-//	@Override
-//	public void setImageDrawable(Drawable drawable)  {
-//		throw (new RuntimeException("Use setTextAndImageResource for setting an image."));
-//	}
-
-	@Override
-	public void setImageURI(Uri uri) {
-		throw (new RuntimeException("Use setTextAndImageResource for setting an image."));
-	}
-
-	
-//	@Override
-//	public void setImageBitmap(Bitmap bm) {
-//		throw (new RuntimeException("Use setTextAndImageResource for setting an image."));
-//	}
-
-	@Override
-	public void setImageResource(int resId) {
-		throw (new RuntimeException("Use setTextAndImageResource for setting an image."));
-	}
-
-	
-	private void setImageResourceInternal(int resId) {
-		Paint paint = new Paint();
-		paint.setColor(this.textColor);
+	private void recalculateTextSize(String text, int textSize) {
+		if (paint == null) {
+			paint = new Paint();
+			paint.setColor(DEFAULTTEXTCOLOR);
+		}
+		// Calculate the text size
 		paint.setTextSize(this.textSize);
 		Rect bounds = new Rect();
+		paint.getTextBounds(this.text, 0, this.text.length(), bounds);
+		width = bounds.width();
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Set the label text color.
+	 * 
+	 * @param color
+	 *            the new text color
+	 */
+	public void setTextColor(int textColor) {
+		if (paint == null) {
+			paint = new Paint();
+		}
+		paint.setColor(textColor);
+	}
+
+	// ------------------------------------------------------------------------
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.widget.ImageView#setImageURI(android.net.Uri)
+	 */
+	@Override
+	public void setImageURI(Uri uri) {
+		throw (new RuntimeException(
+				"Use setTextAndImageResource for setting an image."));
+	}
+
+	// ------------------------------------------------------------------------
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.widget.ImageView#setImageResource(int)
+	 */
+	@Override
+	public void setImageResource(int resId) {
+		throw (new RuntimeException(
+				"Use setTextAndImageResource for setting an image."));
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Internally prepare the drawing of the ImageLabelButton by creating a new
+	 * internal bitmap from the original bitmap but with space for the
+	 * additional text label which is later drawn centered below the original
+	 * icon image.
+	 * 
+	 * @param resId
+	 *            the new image resource internal
+	 */
+	private void setImageResourceInternal(int resId) {
+		Paint paint = new Paint();
+		paint.setTextSize(this.textSize);
+		Rect bounds = new Rect();
+		// Get dummy bounds for the height. Use a dummy text here because
+		// otherwise we get different values for different characters.
 		paint.getTextBounds("XXX", 0, "XXX".length(), bounds);
 		height = bounds.height();
 
-		Log.d("communicator",
-				"# # # TEXT '"+text+"' BOUNDS w=" +bounds.width() + ", h=" + bounds.height());
-
 		Bitmap icon = BitmapFactory.decodeResource(this.getResources(), resId);
 
-		Log.d("communicator",
-				"# # #  ICON  BOUNDS w=" +icon.getWidth() + ", h=" + icon.getHeight());
+		Bitmap icon2 = Bitmap.createBitmap(icon.getWidth(), icon.getHeight()
+				+ height + height, Bitmap.Config.ARGB_8888);
 
-        Bitmap icon2 = Bitmap.createBitmap(icon.getWidth(), icon.getHeight() + height +   height, Bitmap.Config.ARGB_8888);
+		Canvas canvas = new Canvas(icon2);
 
-		Log.d("communicator",
-				"# # #  ICON2  BOUNDS w=" +icon.getWidth() + ", h=" + icon.getHeight() + height +  height);
+		// Draw the source image centered
+		canvas.drawBitmap(icon, 0, 0, new Paint());
 
-        Canvas canvas = new Canvas(icon2);
-        // draw the source image centered
-        canvas.drawBitmap(icon, 0, 0, new Paint());
-		
 		super.setImageBitmap(icon2);
 	}
-	
-	
-	
 
+	// ------------------------------------------------------------------------
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.widget.ImageView#onDraw(android.graphics.Canvas)
+	 */
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 
-		// Calc text size
-		Paint paint = new Paint();
-		paint.setColor(this.textColor);
-		paint.setTextSize(this.textSize);
-		Rect bounds = new Rect();
-		paint.getTextBounds(this.text, 0, this.text.length(), bounds);
-		
-		
-		// Draw the Text
+		// Draw the Text. The space was created in setImageResourceInternal()
+		// already.
 		float buttonWitdh = this.getWidth();
 		float buttonHeight = this.getHeight();
-		canvas.drawText(this.text, (buttonWitdh - bounds.width()) / 2,
-				buttonHeight - height - height/2, paint);
+		canvas.drawText(this.text, (buttonWitdh - width) / 2, buttonHeight
+				- height - height / 2, paint);
 	}
+
+	// ------------------------------------------------------------------------
 
 }
