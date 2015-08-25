a98dd1dbb5895e2b025a8265fa08ce73b4dcf880
delphino <delphino@31b658cd-b084-5547-90eb-9404aba538b9>
2015-08-25 16:25:00 +0000

    ScrollViewEx revised

--- org.cryptocator/src/org/cryptocator/ScrollViewEx.java
+++ org.cryptocator/src/org/cryptocator/ScrollViewEx.java
@@ -36,28 +36,188 @@ package org.cryptocator;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.View;
 import android.widget.ScrollView;
 
+/**
+ * The ScrollViewEx class is special scroll view that allows for detecting
+ * scroll rest. It further allows to lock the scroll position and restore it at
+ * a later point in time. It can even used to detect an overscroll event.
+ * 
+ * @author Christian Motika
+ * @since 1.2
+ * @date 08/23/2015
+ * 
+ */
 public class ScrollViewEx extends ScrollView {
+
+	/** The locked position. */
 	int lockedPos = -1;
 
+	/** The last overscroll. */
 	long lastOverscroll = 0;
+
+	/** The overscroll counter. */
 	int overscrollCnt = 0;
 
+	/** The overscrollmin minimum counter for triggering the overscroll even. */
 	private static int OVERSCROLLMIN = 18;
+
+	/**
+	 * The time in ms in which the overscrolling must take place and the minimum
+	 * time between two consicutive overscrollings.
+	 */
 	private static int LASTOVERSCROLLMINDIFF = 1000;
+
+	/** The scroll view ex listener. */
+	private ScrollViewExListener scrollViewExListener = null;
+
+	/** The watch dog thread is necessary to detect the rest of the scrolling. */
+	public WatchDogThread watchDogThread = null;
+
+	/**
+	 * The time in ms after which the scroll rest event is triggered after
+	 * scrolling took place.
+	 */
+	private static int RESTTIMETRIGGER = 500;
 
 	// -------------------------------------------------------------------------
 
+	/**
+	 * The listener interface for receiving scrollViewEx events. The class that
+	 * is interested in processing a scrollViewEx event implements this
+	 * interface, and the object created with that class is registered with a
+	 * component using the component's
+	 * <code>addScrollViewExListener<code> method. When
+	 * the scrollViewEx event occurs, that object's appropriate
+	 * method is invoked.
+	 * 
+	 * @see ScrollViewExEvent
+	 */
+	public interface ScrollViewExListener {
+
+		/**
+		 * On scroll changed.
+		 * 
+		 * @param scrollView
+		 *            the scroll view
+		 * @param x
+		 *            the x
+		 * @param y
+		 *            the y
+		 * @param oldx
+		 *            the oldx
+		 * @param oldy
+		 *            the oldy
+		 */
+		void onScrollChanged(ScrollViewEx scrollView, int x, int y, int oldx,
+				int oldy);
+
+		/**
+		 * On scroll rested.
+		 * 
+		 * @param scrollView
+		 *            the scroll view
+		 * @param x
+		 *            the x
+		 * @param y
+		 *            the y
+		 * @param oldx
+		 *            the oldx
+		 * @param oldy
+		 *            the oldy
+		 */
+		void onScrollRested(ScrollViewEx scrollView, int x, int y, int oldx,
+				int oldy);
+
+		/**
+		 * On oversroll.
+		 * 
+		 * @param up
+		 *            the up
+		 */
+		void onOversroll(boolean up);
+
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Instantiates a new scroll view ex.
+	 * 
+	 * @param context
+	 *            the context
+	 */
+	public ScrollViewEx(Context context) {
+		super(context);
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Instantiates a new scroll view ex.
+	 * 
+	 * @param context
+	 *            the context
+	 * @param attrs
+	 *            the attrs
+	 * @param defStyle
+	 *            the def style
+	 */
+	public ScrollViewEx(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Instantiates a new scroll view ex.
+	 * 
+	 * @param context
+	 *            the context
+	 * @param attrs
+	 *            the attrs
+	 */
+	public ScrollViewEx(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Sets the scroll view listener.
+	 * 
+	 * @param scrollViewExListener
+	 *            the new scroll view listener
+	 */
+	public void setScrollViewListener(ScrollViewExListener scrollViewExListener) {
+		this.scrollViewExListener = scrollViewExListener;
+	}
+
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Lock position.
+	 */
 	public void lockPosition() {
 		lockedPos = this.getScrollY();
 	}
 
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Checks if is locked.
+	 * 
+	 * @return true, if is locked
+	 */
 	public boolean isLocked() {
 		return lockedPos != -1;
 	}
 
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Restore locked position.
+	 */
 	public void restoreLockedPosition() {
 		final int lockedPosCopy = lockedPos;
 		lockedPos = -1;
@@ -69,44 +229,19 @@ public class ScrollViewEx extends ScrollView {
 		}, 200);
 	}
 
-	public interface ScrollViewExListener {
-		void onScrollChanged(ScrollViewEx scrollView, int x, int y, int oldx,
-				int oldy);
+	// -------------------------------------------------------------------------
 
-		void onScrollRested(ScrollViewEx scrollView, int x, int y, int oldx,
-				int oldy);
-
-		void onOversroll(boolean up);
-
-	}
-
-	private ScrollViewExListener scrollViewExListener = null;
-
-	public ScrollViewEx(Context context) {
-		super(context);
-	}
-
-	public ScrollViewEx(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-	}
-
-	public ScrollViewEx(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
-
-	public void setScrollViewListener(ScrollViewExListener scrollViewExListener) {
-		this.scrollViewExListener = scrollViewExListener;
-	}
-
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.view.View#onScrollChanged(int, int, int, int)
+	 */
 	@Override
 	protected void onScrollChanged(int l, int t, int oldl, int oldt) {
 		super.onScrollChanged(l, t, oldl, oldt);
 
 		if (scrollViewExListener != null && oldt == t) {
 			overscrollCnt++;
-
-//			Log.d("communicator", "######## INC overscrollCnt = "
-//					+ overscrollCnt);
 
 			long tmp = System.currentTimeMillis();
 			if (overscrollCnt > OVERSCROLLMIN) {
@@ -115,15 +250,11 @@ public class ScrollViewEx extends ScrollView {
 				}
 				lastOverscroll = tmp;
 				overscrollCnt = 0;
-//				Log.d("communicator", "######## RST1 overscrollCnt = "
-//						+ overscrollCnt);
 			} else if (tmp - lastOverscroll <= LASTOVERSCROLLMINDIFF) {
 				overscrollCnt = 0;
 			}
 
 		} else {
-//			Log.d("communicator", "######## RST2 overscrollCnt = "
-//					+ overscrollCnt);
 			overscrollCnt = 0;
 		}
 
@@ -131,20 +262,29 @@ public class ScrollViewEx extends ScrollView {
 			scrollViewExListener.onScrollChanged(this, l, t, oldl, oldt);
 		}
 		if (watchDogThread == null) {
-			startWatchDogThread(this.getContext(), 500, scrollViewExListener,
-					this);
+			startWatchDogThread(this.getContext(), RESTTIMETRIGGER,
+					scrollViewExListener, this);
 		}
 		watchDogThread.xCurrent = l;
-		// Log.d("communicator", "@@@@ internal onScrollChanged -> " + t);
 		watchDogThread.yCurrent = t;
-		// reset our watchdog
+		// Reset our watchdog
 		watchDogThread.reset();
 	}
 
-	// ---
+	// -------------------------------------------------------------------------
 
-	public WatchDogThread watchDogThread = null;
-
+	/**
+	 * Start watch dog thread.
+	 * 
+	 * @param context
+	 *            the context
+	 * @param seconds
+	 *            the seconds
+	 * @param listener
+	 *            the listener
+	 * @param scrollView
+	 *            the scroll view
+	 */
 	public void startWatchDogThread(Context context, int seconds,
 			ScrollViewExListener listener, ScrollViewEx scrollView) {
 		if (watchDogThread == null) {
@@ -158,26 +298,72 @@ public class ScrollViewEx extends ScrollView {
 		}
 	}
 
+	// -------------------------------------------------------------------------
+
+	/**
+	 * Stop watch dog thread.
+	 */
 	public void stopWatchDogThread() {
 		if (watchDogThread != null) {
 			watchDogThread.cancel();
 		}
 	}
 
+	// -------------------------------------------------------------------------
+
+	/**
+	 * The internal class WatchDogThread is responsible for detecting a rest of
+	 * the scrolling.
+	 */
 	class WatchDogThread extends Thread {
+
+		/** The context. */
 		Context context;
+
+		/** The cancel. */
 		boolean cancel = false;
+
+		/** The start or reset time stamp. */
 		long startOrResetTimeStamp = 0;
+
+		/** The enabled. */
 		boolean enabled = false;
+
+		/** The milliseconds. */
 		int milliseconds = 0;
+
+		/** The listener. */
 		ScrollViewExListener listener = null;
+
+		/** The scroll view. */
 		ScrollViewEx scrollView = null;
 
+		/** The x last rest. */
 		public int xLastRest;
+
+		/** The y last rest. */
 		public int yLastRest;
+
+		/** The x current. */
 		public int xCurrent;
+
+		/** The y current. */
 		public int yCurrent;
 
+		// -------------------------------------------------
+
+		/**
+		 * Instantiates a new watch dog thread.
+		 * 
+		 * @param context
+		 *            the context
+		 * @param milliseconds
+		 *            the milliseconds
+		 * @param listener
+		 *            the listener
+		 * @param scrollView
+		 *            the scroll view
+		 */
 		public WatchDogThread(Context context, int milliseconds,
 				ScrollViewExListener listener, ScrollViewEx scrollView) {
 			super();
@@ -187,15 +373,32 @@ public class ScrollViewEx extends ScrollView {
 			this.scrollView = scrollView;
 		}
 
+		// -------------------------------------------------
+
+		/**
+		 * Reset.
+		 */
 		public void reset() {
 			startOrResetTimeStamp = System.currentTimeMillis();
 			enabled = true;
 		}
 
+		// -------------------------------------------------
+
+		/**
+		 * Cancel.
+		 */
 		public void cancel() {
 			cancel = true;
 		}
 
+		// -------------------------------------------------
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Thread#run()
+		 */
 		public void run() {
 			while (!cancel) {
 				try {
@@ -215,7 +418,7 @@ public class ScrollViewEx extends ScrollView {
 						scrollView.overscrollCnt = 0;
 						Log.d("communicator", "######## RST3 overscrollCnt = "
 								+ overscrollCnt);
-						// wait for the next scrolling even which does a reset()
+						// Wait for the next scrolling even which does a reset()
 						// call enabling the watchdog again
 						enabled = false;
 					}
@@ -224,5 +427,7 @@ public class ScrollViewEx extends ScrollView {
 			watchDogThread = null;
 		}
 	}
+
+	// ------------------------------------------------------------------------
 
 }
